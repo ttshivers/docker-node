a07cf8b266bb499eee56abd9e5991d079147f699
Travis Shivers <ttshivers@gmail.com>
2020-09-23 17:33:30 -0500

    fix payload

--- prFormat.js
+++ prFormat.js
@@ -12,20 +12,20 @@ module.exports = async ({ github, context: { payload }, maintainersFile }) => {
   console.log('payload', payload);
   // https://developer.github.com/v3/search/#search-issues-and-pull-requests
   const { total_count, items: [pr] } = await github.search.issuesAndPullRequests({
-    q: `${payload.event.head_commit.id}+repo:${payload.event.repository.full_name}+type:pr+is:merged`,
+    q: `${payload.head_commit.id}+repo:${payload.repository.full_name}+type:pr+is:merged`,
   });
 
   // Use pr title if available or fallback to first line of head commit message
   const title = total_count
     ? pr.title
-    : payload.event.head_commit.message.split('\n')[0];
+    : payload.head_commit.message.split('\n')[0];
 
   const prTitle = `Node: ${title}`;
 
   // Link to either the pull request or commit range
   const refMessage = total_count
-    ? `Pull Request: ${payload.event.repository.full_name}#${pr.number}`
-    : `Commit: ${payload.event.repository.html_url}/compare/${payload.event.before}...${payload.event.after}`;
+    ? `Pull Request: ${payload.repository.full_name}#${pr.number}`
+    : `Commit: ${payload.repository.html_url}/compare/${payload.before}...${payload.after}`;
 
   const maintainersPart = await readMaintainers(maintainersFile);
 
